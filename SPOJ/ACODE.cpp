b20cef353645af81871b4cd3fe9b4e1d06a43084
ujjwal <ujjwal01kumar@gmail.com>
2017-03-28 21:00:02 +0530

    Nice DP(take care of internal 0s)

--- SPOJ/ACODE.cpp
+++ SPOJ/ACODE.cpp
@@ -3,20 +3,31 @@ typedef unsigned long long ll;
 using namespace std;
 
 int main(int argc, char const *argv[])
-{
-	int l,t;string s;
+{	
+	std::ios::sync_with_stdio(false);
+
+	int l;
+	string s;
 	while(1){
 		cin>>s;
-		if(s=="0")
-			return 0;
+		if(s[0]=='0')
+			break;
 		l = s.length();
-		ll dp[l+1];
-		dp[0]=1;dp[1]=1;
-		for(int i=2;i<=l;i++){
-			dp[i]=dp[i-1];
-			t = 10*(s[i-2])
+		vector<ll> dp(l,0);
+		dp[0]=1;
+		int q;
+		for(int i=1;i<l;i++){
+			q = 10*(s[i-1]-'0') + (s[i]-'0');
+			if(s[i]-'0')
+				dp[i] = dp[i-1];
+			if(q>=10 && q<=26){
+				if(i==1)
+					dp[i] = dp[i]+1;
+				else
+					dp[i] = dp[i]+dp[i-2];
+			}
 		}
-
+		cout<<dp[l-1]<<"\n";
 	}
 	return 0;
 }
