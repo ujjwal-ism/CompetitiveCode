27dc3dc62b7a1b5794d24ed9bddfe20f714ce8f0
ujjwal <ujjwal01kumar@gmail.com>
2017-04-03 20:54:52 +0530

    Game Theory

---
+++ SPOJ/SHAKTI.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t,n;scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		(n%2==0)?printf("Thankyou Shaktiman\n"):printf("Sorry Shaktiman\n");
+	}
+	return 0;
+}
