6aa84abdf9e3497378125759b11ae2c9fcf0d4e7
ujjwal <ujjwal01kumar@gmail.com>
2017-05-24 17:49:22 +0530

    Sliding window -- rather two pointer method

---
+++ SPOJ/ALIEN.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ll;
+int main(int argc, char const *argv[])
+{
+	std::ios::sync_with_stdio(false);cin.tie(NULL);
+
+	int t;cin>>t;
+	int n;
+	ll b;
+	while(t--){
+		cin>>n>>b;
+		vector<int> v(n);
+		for(int i=0;i<n;i++)
+			cin>>v[i];
+		ll a = 0; // humans
+		int s = 0; // stations
+		int i = 0,j=1;
+		ll q = v[0];
+		int t;
+		while(j<n){
+			if(q + v[j] > b)
+				break;
+			q += v[j];
+			j++;
+		}
+		s = j-i;
+		a = q;
+		q -= v[i];
+		i++;
+		while(j<n){
+			if(q+v[j] > b){
+				if(j-i > s){
+					a = q;
+					s = j-i;
+				}
+				else if(j-i == s && q < a){
+					a = q;
+				}
+				q -= v[i];i++;
+			}
+			q += v[j];
+			j++;
+		}
+		if(j-i > s){
+			a = q;
+			s = j-i;
+		}
+		else if(j-i == s && q < a && i!=0){
+			a = q;
+		}
+		cout<<a<<" "<<s<<"\n";
+
+	}
+	return 0;
+}
