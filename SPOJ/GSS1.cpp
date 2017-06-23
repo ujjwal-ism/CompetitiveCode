2eb6c4d8d8e8b1dceffacadd81178ce601636b93
ujjwal <ujjwal01kumar@gmail.com>
2017-06-23 19:14:43 +0530

    segment tree

--- SPOJ/GSS1.cpp
+++ SPOJ/GSS1.cpp
@@ -1,44 +1,62 @@
 #include <bits/stdc++.h>
-
+#define MAX 50005
+#define MIN -1000000000
 using namespace std;
-typedef long long ll;
-int n;  // array size
-ll t[50001];
+typedef long ll;
 
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
-}
+struct node{
+  ll ls;
+  ll rs;
+  ll ts;
+  ll ms;
+};
 
-ll query(int l, int r) {  // sum on interval [l, r)
-  ll res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
+ll arr[MAX];
+node tree[4*MAX];
+
+void build(int ci,int s,int e){
+  if(s == e){
+    tree[ci] = ((node){arr[s],arr[s],arr[s],arr[s]});
+    return ;
   }
-  return res;
-}
-
-int main() {
-  scanf("%d", &n);
-  for (int i = 0; i < n; ++i) scanf("%lld", t + n + i);
-  build();
-  int m;scanf("%d",&m);
-  while(m--){		
-  int x,y;scanf("%d%d",&x,&y);
-  if(n==1)
-  	printf("%lld",t[1]);
   else{
-  	ll t;
-  	ll ans = query(x-1,x);
-  	for(int i = x-1;i<=y;i++){
-  		for(int j = i;j<=y;j++){
-  			t = query(i,j);
-  			if(t>ans)
-  				ans = t;
-  		}
-  	}
-  	printf("%lld",ans);
+    int mid = (s+e)>>1;
+    build(2*ci+1,s,mid);
+    build(2*ci+2,mid+1,e);
+    node l = tree[2*ci+1] , r = tree[2*ci+2];
+    tree[ci].ts = l.ts + r.ts;
+    tree[ci].ls = max(l.ls,l.ts + r.ls);
+    tree[ci].rs = max(r.rs,r.ts + l.rs);
+    tree[ci].ms = max(l.ms,max(r.ms,l.rs+r.ls)); 
   }
 }
+
+node query(int ci,int s,int e,int i,int j){
+  if(s > e || s >j || e < i){
+    return ((node){MIN,MIN,MIN,MIN});
+  }
+  if(s >= i && e <= j)
+    return tree[ci];
+  
+  int mid = (s+e)>>1;
+  node l = query(2*ci+1,s,mid,i,j);
+  node r = query(2*ci+2,mid+1,e,i,j);
+
+  return ((node){max(l.ls,l.ts + r.ls),max(r.rs,r.ts + l.rs),l.ts + r.ts,max(l.ms,max(r.ms,l.rs+r.ls))});
+}
+
+int main()
+{
+  int n,i,j;scanf("%d",&n);
+  for(int i=0;i<n;i++)
+    scanf("%ld",arr+i);
+
+  build(0,0,n-1);
+
+  ll q;scanf("%ld",&q);
+  while(q--){
+    scanf("%d%d",&i,&j);
+    printf("%ld\n",query(0,0,n-1,i-1,j-1).ms);
+  }
   return 0;
 }
