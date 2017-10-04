bbe056b49566b7ddff378544f8dc0477c4d77af5
ujjwal <ujjwal01kumar@gmail.com>
2017-10-04 14:11:49 +0530

    ad-hoc (STL)

--- SPOJ/ABSYS.cpp
+++ SPOJ/ABSYS.cpp
@@ -5,26 +5,27 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int t,l;cin>>t;
-	string s;
+	char a[10],b[10],c[10],plus[10],equal[10];
 	while(t--){
-		cin>>s;
-		l = s.length();
-		vector<int> v;
-		for(int i=0;i<l;i++){
-			if(isspace(s[i]))
-				v.push_back(i);
+		cin>>a>>plus>>b>>equal>>c;
+		if(strstr(a,"machula")){
+			int x = stoi(b);
+			int z = stoi(c);
+			l = z - x;
+			cout<<l<<" + "<<x<<" = "<<z<<"\n";
 		}
-		cout<<v.size()<<endl;
-		string s1,s2,s3;
-		int l1,l2;
-
-		l1 = v[2]-v[1];
-			cout<<l1;
-		l2 = l-v[3];
-		s1 = s.substr(0,v[0]);
-		s2 = s.substr(v[1],l1);
-		s3 = s.substr(v[3],l2);
-		cout<<s1<<" "<<s2<<" "<<s3<<endl;
+		else if(strstr(b,"machula")){
+			int x = stoi(a);
+			int z = stoi(c);
+			l = z - x;
+			cout<<x<<" + "<<l<<" = "<<z<<"\n";
+		}
+		else{
+			int x = stoi(a);
+			int z = stoi(b);
+			l = z + x;
+			cout<<x<<" + "<<z<<" = "<<l<<"\n";
+		}
 	}
 	return 0;
 }
