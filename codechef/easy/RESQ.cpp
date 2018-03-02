ba589890805b0abe9e9d7efc6aa5a972f0b8557f
ujjwal <ujjwal01kumar@gmail.com>
2018-03-02 18:17:05 +0530

    brute force

---
+++ codechef/easy/RESQ.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+int main(int argc, char const *argv[])
+{
+   	std::ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);		
+	int t;cin>>t;
+	while(t--) {
+		int n;cin>>n;
+		int mn = INT_MAX;
+
+		int l = sqrt(n);
+		for(int i=1;i <= l; i++) {
+			if(n%i == 0) {
+				mn = min(mn, (n/i) - i);
+			}
+		}
+
+		cout<<mn<<endl;
+	}
+
+	return 0;
+}
